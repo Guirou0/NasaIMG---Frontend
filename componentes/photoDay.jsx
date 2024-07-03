cace7237c7abf009097ce57639deec18df7abb02
Guirou0 <guirou112@gmail.com>
2024-07-03 16:23:41 -0300

    Adicionando arquivos

---
+++ componentes/photoDay.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import styles from "../modulescss/photoDay.module.css";
+
+
+const PhotoDay = () => {
+    return(
+        <>
+            Hello
+        </>
+    )
+}
+
+export default PhotoDay
---
+++ componentes/photoPast.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import styles from "../modulescss/photoPast.module.css";
+
+
+
+const PhotoPast = () => {
+    return(
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default PhotoPast
